3bc5c66db3e13f03304b438973b67a2b12b85532
rferdouss <84193541+rferdouss@users.noreply.github.com>
2022-05-25 23:39:16 +0200

    Update JaccardDistance.java

--- src/main/java/eu/fbk/iv4xr/rlbt/labrecruits/distance/JaccardDistance.java
+++ src/main/java/eu/fbk/iv4xr/rlbt/labrecruits/distance/JaccardDistance.java
@@ -96,13 +96,71 @@ public class JaccardDistance implements StateDistance {
 			if(numcommonFeature == totalFeatureS1) {
 				flagsubsume = true;
 			}
+			/*else {
+				subsumeratio = (numcommonFeature/totalFeatureS1);
+				if (subsumeratio >= subsumethreshold)
+					flagsubsume = true;
+			}*/
 		}
 		//if(numcommonFeature==0)
 		//	System.out.println("subsume()- no common entities");
 		
-		//System.out.println("subsume() - total feature in sq = "+ totalFeatureS1+"  intersect = "+ numcommonFeature+"  subsumeFag = "+flagsubsume);
-		
+		System.out.println("subsume()-tot feature= "+ totalFeatureS1+"  comm= "+ numcommonFeature+"  subsumeFag = "+flagsubsume);		
 		return flagsubsume;
+	}
+	
+	@Override
+	public double statesimilarity(State s1, State s2) {  // if state s1 is subsumed in state s2
+		double subsumeratio=0;
+		double totalFeatureS1=0;
+		double numcommonFeature =0;
+		double subsumethreshold =0.9;  //80% match
+		
+		LabRecruitsState labRecruitsState1 = (LabRecruitsState)s1;
+		LabRecruitsState labRecruitsState2 = (LabRecruitsState)s2;
+		//System.out.println("in subsume()-s1 = "+ labRecruitsState1.toString());
+		//System.out.println("in subsume()-s2 = "+ labRecruitsState2.toString());
+		//totalFeatureS1 = labRecruitsState1.getObjectsMap().keySet().size();//+ labRecruitsState2.getObjectsMap().keySet().size();
+		for (String k : labRecruitsState1.getObjectsMap().keySet()) 
+		{
+			LabRecruitsEntityObject entity1 = (LabRecruitsEntityObject) labRecruitsState1.getObjectsMap().get(k);
+			if (entity1.getLabRecruitsEntity().type =="Switch") 
+				totalFeatureS1 = totalFeatureS1+1;
+			
+			if (labRecruitsState2.getObjectsMap().containsKey(k)) 
+			{				
+				LabRecruitsEntityObject entity2 = (LabRecruitsEntityObject) labRecruitsState2.getObjectsMap().get(k);
+				//System.out.println("in subsume()-entity id= "+entity1.getLabRecruitsEntity().id+"   type="+entity1.getLabRecruitsEntity().type);
+				if (entity1.getLabRecruitsEntity().type =="Switch") 
+				{
+					//System.out.println("inside loop-entity is switch= "+entity1.getLabRecruitsEntity().type);
+					String entitybooleanproperty = getbooleanpropertystring(entity1.getLabRecruitsEntity().type);
+					if (entity1.getLabRecruitsEntity().getBooleanProperty(entitybooleanproperty)== entity2.getLabRecruitsEntity().getBooleanProperty(entitybooleanproperty)) 
+					{
+						numcommonFeature++;
+					}
+				}
+			}			
+			//System.out.println(labRecruitsState1.getObjectsMap().get(k).name());
+		}
+		if(numcommonFeature>0 && totalFeatureS1>0) 
+		{
+			if(numcommonFeature == totalFeatureS1) {
+				subsumeratio = 1.0;
+				return subsumeratio;
+			}
+			else {
+				subsumeratio = (numcommonFeature/totalFeatureS1);
+				if (subsumeratio >= subsumethreshold)
+					return subsumeratio;
+			}
+		}
+		//if(numcommonFeature==0)
+		//	System.out.println("subsume()- no common entities");
+		
+		System.out.println("subsume()-tot feature= "+ totalFeatureS1+"  comm= "+ numcommonFeature+" ratio= "+subsumeratio);
+		
+		return subsumeratio;
 	}
 	
 	
