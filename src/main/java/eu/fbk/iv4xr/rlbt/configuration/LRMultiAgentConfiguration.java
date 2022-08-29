ef210ddc018258a6a04fb7ffd21679ea2cea984f
rferdouss <84193541+rferdouss@users.noreply.github.com>
2022-08-29 12:39:08 +0200

    configuration for multi-agent env

---
+++ src/main/java/eu/fbk/iv4xr/rlbt/configuration/LRMultiAgentConfiguration.java
@@ -0,0 +1,33 @@
+package eu.fbk.iv4xr.rlbt.configuration;
+
+import java.util.LinkedHashMap;
+
+public class LRMultiAgentConfiguration extends Configuration{
+	
+	
+	public LRMultiAgentConfiguration(){
+		parameters = new LinkedHashMap<String, Object>();
+		parameters.put("labrecruits.level_name", "buttons_doors_1");
+		parameters.put("labrecruits.level_folder", "src/test/resources/levels/");
+		
+		parameters.put("labrecruits.execution_folder", ".");
+		
+		parameters.put("labrecruits.agentpassive_id", "agent1");
+		parameters.put("labrecruits.agentactive_id", "agent2");
+		parameters.put("labrecruits.use_graphics",true);
+		parameters.put("labrecruits.max_ticks_per_action",100);
+		parameters.put("labrecruits.max_actions_per_episode",500);
+		parameters.put("labrecruits.target_entity_name","door3");
+		parameters.put("labrecruits.target_entity_type","DOOR");
+		parameters.put("labrecruits.target_entity_property_name","isOpen");
+		parameters.put("labrecruits.target_entity_property_value",true);
+		parameters.put("labrecruits.search_mode", "GoalOriented");		
+		parameters.put("labrecruits.functionalCoverage", true);
+		parameters.put("labrecruits.max_actions_since_last_new_state", 100);
+		parameters.put("labrecruits.rewardtype", "CuriousityDriven");
+		parameters.put("labrecruits.testingsession", false);
+		parameters.put("labrecruits.memory_clean_interval_action", 2);
+		parameters.put("labrecruits.exploreEventOn", true);
+	}
+	
+}
